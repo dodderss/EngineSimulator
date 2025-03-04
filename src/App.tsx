e857d4eeaa76f84c1d7dfb2b39d5e2783fca5371
William <69767121+dodderss@users.noreply.github.com>
2025-03-04 11:54:59 +0000

    chore: removed API key from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ function App() {
     if (!haveRunCurrencyConversion) {
       setHaveRunCurrencyConversion(true);
       const apiURL =
-        "https://api.currencyapi.com/v3/latest?apikey=cur_live_bH0kwrMh4TxV9St24d1L1AIw9Z3dxBrWrydSiSuV&currencies=USD%2CEUR%2CJPY&base_currency=GBP";
+        "https://api.currencyapi.com/v3/latest?apikey=&currencies=USD%2CEUR%2CJPY&base_currency=GBP";
       fetch(apiURL)
         .then((response) => response.json())
         .then((data) => {
